70071d648d9adbb7045e19e9402293bf92f30fce
sacha-renault <sacha35340@gmail.com>
2026-02-25 14:00:41 +0100

    refactor: rename ParamBuilder into ParamViewBuilder

--- crates/hug-derive/src/lib.rs
+++ crates/hug-derive/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 
 mod param_builder;
 
-#[proc_macro_derive(ParamBuilder, attributes(builder))]
+#[proc_macro_derive(ParamViewBuilder, attributes(builder))]
 pub fn derive_param_builder(input: TokenStream) -> TokenStream {
     param_builder::derive_param_builder(input)
 }
