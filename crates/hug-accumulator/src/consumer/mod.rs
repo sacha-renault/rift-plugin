41d7f522b651b0926cfea08763918d7f727743cb
sacha-renault <sacha35340@gmail.com>
2026-03-02 14:01:14 +0100

    feat: encapsulate acc info into structs

--- crates/hug-accumulator/src/consumer/mod.rs
+++ crates/hug-accumulator/src/consumer/mod.rs
@@ -1,3 +1,5 @@
+use crate::{BlockTime, accumulator::ChannelsInfo};
+
 pub trait AudioConsumer: Send + Sync + 'static {
-    fn consume(&mut self, block: &[f32], channel_idx: usize, total_channels: usize);
+    fn consume(&mut self, block: &[f32], channel_info: ChannelsInfo, time: BlockTime);
 }
