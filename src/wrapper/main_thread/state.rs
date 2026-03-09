66f0d73993857dde06452c5ecdb0014f34bade27
sacha-renault <sacha35340@gmail.com>
2026-03-09 14:26:08 +0100

    fix: usze serde_json as it makes it easier to preserve order in save parameters

--- src/wrapper/main_thread/state.rs
+++ src/wrapper/main_thread/state.rs
@@ -1,10 +1,8 @@
-use std::collections::HashMap;
 use std::io::{Read, Write};
 
 use clack_extensions::state::PluginStateImpl;
 use clack_plugin::prelude::*;
 use clack_plugin::stream::{InputStream, OutputStream};
-use tinyjson::JsonValue;
 
 use crate::prelude::*;
 use crate::wrapper::ClapPlugin;
@@ -16,18 +14,12 @@ impl<'a, P: ClapPlugin> PluginStateImpl for super::WrapperMainThread<'a, P> {
             .read_to_end(&mut bytes)
             .map_err(|_| PluginError::Message("Failed to read state"))?;
 
-        let json: JsonValue = String::from_utf8(bytes)
-            .map_err(|_| PluginError::Message("Failed to read state as UTF-8"))?
-            .parse()
+        let root: serde_json::Map<String, serde_json::Value> = serde_json::from_slice(&bytes)
             .map_err(|_| PluginError::Message("Failed to parse state as JSON"))?;
-
-        let root = json
-            .get::<HashMap<_, _>>()
-            .ok_or(PluginError::Message("Expected JSON object"))?;
 
         let params_map = root
             .get("params")
-            .and_then(|v| v.get::<HashMap<_, _>>())
+            .and_then(|v| v.as_object())
             .ok_or(PluginError::Message("Missing params"))?;
 
         let params = &self.shared.params;
@@ -35,13 +27,10 @@ impl<'a, P: ClapPlugin> PluginStateImpl for super::WrapperMainThread<'a, P> {
             let plain_id: u32 = id
                 .parse()
                 .map_err(|_| PluginError::Message("Invalid param id"))?;
-            let id = ClapId::new(plain_id);
-
             let value = value
-                .get::<f64>()
+                .as_f64()
                 .ok_or(PluginError::Message("Invalid param value"))?;
-
-            params.set_value(id, *value);
+            params.set_value(ClapId::new(plain_id), value);
         }
 
         Ok(())
@@ -51,37 +40,33 @@ impl<'a, P: ClapPlugin> PluginStateImpl for super::WrapperMainThread<'a, P> {
         let params = &self.shared.params;
         let count = params.count();
 
-        let mut params_map: HashMap<String, JsonValue> = HashMap::new();
+        let mut params_vec: Vec<(String, serde_json::Value)> = Vec::new();
         for i in 0..count {
             let info = params
                 .get_param_info(i)
                 .ok_or(PluginError::Message("Not supposed to happen"))?;
-            let id = info.id;
             let value = params
-                .get_value(id)
+                .get_value(info.id)
                 .ok_or(PluginError::Message("Not supposed to happen"))?;
-            params_map.insert(id.to_string(), JsonValue::Number(value));
+            params_vec.push((info.id.to_string(), serde_json::Value::from(value)));
         }
 
-        log::info!("param_save");
-
-        let mut root: HashMap<String, JsonValue> = HashMap::new();
+        let mut root = serde_json::Map::new();
         root.insert(
             "version".to_string(),
-            JsonValue::String(P::VERSION.to_string()),
+            serde_json::Value::String(P::VERSION.to_string()),
         );
-        root.insert("params".to_string(), JsonValue::Object(params_map));
+        root.insert(
+            "params".to_string(),
+            serde_json::Value::Object(params_vec.into_iter().collect()),
+        );
 
-        let json = JsonValue::Object(root);
+        let json = serde_json::to_string(&root)
+            .map_err(|_| PluginError::Message("Failed to serialize state"))?;
+
         output
-            .write_all(
-                json.stringify()
-                    .map_err(|_| PluginError::Message("Failed to serialize state"))?
-                    .as_bytes(),
-            )
+            .write_all(json.as_bytes())
             .map_err(|_| PluginError::Message("Failed to write state"))?;
-
-        log::info!("write");
 
         Ok(())
     }
