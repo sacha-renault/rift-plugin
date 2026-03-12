32975361dee8b6366fcba56d0852a4ebe80f1440
Sacha RENAULT <sacha35340@gmail.com>
2026-03-12 14:12:16 +0100

    feat: create a texture cache

--- crates/rift-plugin-vizia/src/utils/mod.rs
+++ crates/rift-plugin-vizia/src/utils/mod.rs
@@ -1,12 +1,14 @@
 //! Contains utility structs and functions. Should be used in
 //! [`super::elements`]
 
+mod cached_texture;
 mod draw_utils;
 mod gui_events;
 mod handle_generic_extensions;
 mod lens;
 mod viewport_transform;
 
+pub use cached_texture::CachedTexture;
 pub use draw_utils::{clip_bounds, make_strokepath};
 pub use gui_events::{gesture_end, gesture_start, set_value, set_value_normalized};
 pub use handle_generic_extensions::{
