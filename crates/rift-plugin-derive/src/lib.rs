168687b73e5de104831503d04930fd499338789b
Sacha RENAULT <sacha35340@gmail.com>
2026-03-12 19:54:20 +0100

    doc: add doc for procedural macro

--- crates/rift-plugin-derive/src/lib.rs
+++ crates/rift-plugin-derive/src/lib.rs
@@ -5,6 +5,29 @@ mod derive_param;
 mod enum_param;
 mod param_builder;
 
+/// Generate boiler plate code for the builder > destructure > build view pattern.
+///
+/// This is meant to be used with `DestructThenBuildView` trait.
+///
+/// # Example:
+/// ```ignore
+/// #[derive(ParamViewBuilder)]
+/// struct MyParamView {
+///     // this value must be passed to contructor (no need for default)
+///     #[builder(new)]
+///     value1: f32
+///     
+///     // Can be added with builder pattern (needs to impl Default or have a default specified)
+///     #[builder(default = 10.)]
+///     value2: f32
+/// }
+///
+/// impl DestructThenBuildView for MyParamView {
+///     fn build_view(self, cx: &mut Context) -> Handle<'_, impl View> { ... }
+/// }
+///
+/// MyParamView::new(1.0).value2(5.0).build_view(cx);
+/// ```
 #[proc_macro_derive(ParamViewBuilder, attributes(builder))]
 pub fn derive_param_builder(input: TokenStream) -> TokenStream {
     param_builder::derive_param_builder(input)
