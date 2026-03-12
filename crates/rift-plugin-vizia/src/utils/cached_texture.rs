4c9fcb4cac148a518ae082b95d7f487a49554580
Sacha RENAULT <sacha35340@gmail.com>
2026-03-12 19:48:21 +0100

    feat: create a texture cache

---
+++ crates/rift-plugin-vizia/src/utils/cached_texture.rs
@@ -0,0 +1,60 @@
+use std::cell::RefCell;
+
+use vizia::prelude::{BoundingBox, Canvas, DrawContext};
+use vizia::vg::{AlphaType, Color, ColorType, ISize, Image, ImageInfo};
+
+pub struct CachedTexture {
+    texture: RefCell<Option<Image>>,
+}
+
+impl CachedTexture {
+    pub fn new() -> Self {
+        Self {
+            texture: RefCell::new(None),
+        }
+    }
+
+    pub fn invalidate(&self) {
+        *self.texture.borrow_mut() = None;
+    }
+
+    pub fn has_cache(&self) -> bool {
+        self.texture.borrow().is_some()
+    }
+
+    pub fn draw_cache(&self, cx: &mut DrawContext, canvas: &mut Canvas, image: &Image) {
+        let BoundingBox { x, y, .. } = cx.bounds();
+        canvas.draw_image(image, (x, y), None);
+    }
+
+    pub fn draw<F>(&self, cx: &mut DrawContext, canvas: &mut Canvas, draw_fn: F)
+    where
+        F: Fn(&mut DrawContext, &Canvas),
+    {
+        if let Some(image) = self.texture.borrow().as_ref() {
+            self.draw_cache(cx, canvas, image);
+        } else if let Some(image) = create_texture(cx, canvas, draw_fn) {
+            self.draw_cache(cx, canvas, &image);
+            *self.texture.borrow_mut() = Some(image);
+        }
+    }
+}
+
+fn create_texture<F>(cx: &mut DrawContext, canvas: &mut Canvas, draw_fn: F) -> Option<Image>
+where
+    F: Fn(&mut DrawContext, &Canvas),
+{
+    let BoundingBox { w, h, .. } = cx.bounds();
+
+    // Create the new texture
+    let size = ISize::new(w.ceil() as i32, h.ceil() as i32);
+    let info = ImageInfo::new(size, ColorType::RGBA8888, AlphaType::Premul, None);
+    let mut surface = canvas.new_surface(&info, None)?;
+    let render_canvas = surface.canvas();
+
+    // Draw in the render canvas
+    render_canvas.clear(Color::TRANSPARENT);
+    draw_fn(cx, render_canvas);
+    let image = surface.image_snapshot();
+    Some(image)
+}
