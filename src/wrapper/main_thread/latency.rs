e8cdcaaae351dc7c8985bbbefb5eba08fe964cd1
sacha-renault <sacha35340@gmail.com>
2026-03-16 17:08:21 +0100

    feat: add debug logs

--- src/wrapper/main_thread/latency.rs
+++ src/wrapper/main_thread/latency.rs
@@ -5,6 +5,7 @@ use crate::wrapper::ClapPlugin;
 
 impl<'a, P: ClapPlugin> PluginLatencyImpl for super::WrapperMainThread<'a, P> {
     fn get(&mut self) -> u32 {
+        log::debug!("get (latency)");
         self.shared.states.get_latency()
     }
 }
