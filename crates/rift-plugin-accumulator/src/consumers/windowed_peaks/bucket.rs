c3e38d2221870e6a798f9726e96a857a7d580417
Sacha RENAULT <sacha35340@gmail.com>
2026-03-17 13:26:31 +0100

    doc (accumulator): add doc on Bucket trait

--- crates/rift-plugin-accumulator/src/consumers/windowed_peaks/bucket.rs
+++ crates/rift-plugin-accumulator/src/consumers/windowed_peaks/bucket.rs
@@ -1,7 +1,23 @@
+/// A trait that allows [`super::window::WindowBuckets`] to works
+/// with any kind of bucket that implement [`Bucket`].
+///
+/// See the implementation of [`super::peaks::PeakBucket`] as example.
 pub trait Bucket: Clone + 'static {
+    /// Create a new bucket with initial value of `x`. Right after this call
+    /// [`Self::count`] must be 1.
     fn new(x: f32) -> Self;
+
+    /// Create a new empty bucket. Right after this call
+    /// [`Self::count`] must be 0.
     fn empty() -> Self;
+
+    /// Add a sample in the current bucket. [`Self::count`] must be
+    /// incremented by 1.
     fn add_sample(&mut self, x: f32);
+
+    /// Get the current value of the bucket.
     fn value(&self) -> f32;
+
+    /// Get the number of sample that were accumulated in this bucket.
     fn count(&self) -> usize;
 }
