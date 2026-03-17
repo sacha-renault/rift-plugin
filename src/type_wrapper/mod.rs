8bcd27de29a2a39827829abd441bb30c26240cb6
sacha-renault <sacha35340@gmail.com>
2026-03-17 19:30:44 +0100

    feat: Plugin trait now have a way to define midi ports pretty easyly

--- src/type_wrapper/mod.rs
+++ src/type_wrapper/mod.rs
@@ -1,5 +1,7 @@
-mod midi;
+mod midi_message;
+mod midi_port;
 mod ports;
 
-pub use midi::{MidiMessage, MidiMessageKind};
+pub use midi_message::{MidiMessage, MidiMessageKind};
+pub use midi_port::MidiPort;
 pub use ports::{AudioPort, MainAudioPort, PAIR_PORT_ID};
