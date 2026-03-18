9e8cca4bd2ca9a819c9ceb4baad7bac2ab8833a6
sacha-renault <sacha35340@gmail.com>
2026-03-18 22:40:47 +0100

    refactor: params goes into shared crate

--- crates/rift-plugin-shared/src/params/param_float.rs
+++ crates/rift-plugin-shared/src/params/param_float.rs
@@ -1,19 +1,21 @@
-use std::sync::atomic::{AtomicI32, Ordering};
+use std::sync::atomic::Ordering;
 
 use clack_extensions::params::*;
 use clack_plugin::utils::ClapId;
 
-use crate::_sealed::__ParamInitializer;
-use crate::prelude::{ClapParam, ParamPtr, TypedParam};
+use super::ptr::ParamPtr;
+use super::traits::{__ParamInitializer, ClapParam, TypedParam};
+
+use super::atomic_f32::AtomicF32;
 
 #[derive(bon::Builder)]
-pub struct IntParam {
+pub struct FloatParam {
     /// Default value for the param
     #[allow(unused)]
-    pub(crate) default: i32,
+    pub(crate) default: f32,
 
-    #[builder(skip = AtomicI32::new(default))]
-    pub(crate) value: AtomicI32,
+    #[builder(skip = AtomicF32::new(default))]
+    pub(crate) value: AtomicF32,
 
     /// The name of the param will
     /// be initialized in the derive with it's clap ID
@@ -27,11 +29,11 @@ pub struct IntParam {
     #[builder(default = "")]
     pub(crate) unit: &'static str,
 
-    #[builder(default = 0)]
-    pub(crate) min_value: i32,
+    #[builder(default = 0.0)]
+    pub(crate) min_value: f64,
 
-    #[builder(default = 1)]
-    pub(crate) max_value: i32,
+    #[builder(default = 1.0)]
+    pub(crate) max_value: f64,
 
     #[builder(default = ParamInfoFlags::IS_AUTOMATABLE)]
     pub(crate) flags: ParamInfoFlags,
@@ -40,8 +42,8 @@ pub struct IntParam {
     pub(crate) id: ClapId,
 }
 
-impl TypedParam for IntParam {
-    type Value = i32;
+impl TypedParam for FloatParam {
+    type Value = f32;
 
     fn value(&self) -> Self::Value {
         self.value.load(Ordering::SeqCst)
@@ -52,7 +54,7 @@ impl TypedParam for IntParam {
     }
 }
 
-impl ClapParam for IntParam {
+impl ClapParam for FloatParam {
     fn name(&self) -> &str {
         &self.name
     }
@@ -70,8 +72,7 @@ impl ClapParam for IntParam {
     }
 
     fn set_raw(&self, value: f64) {
-        let int_value = (value as i32).clamp(self.min_value, self.max_value);
-        self.value.store(int_value, Ordering::SeqCst);
+        self.value.store(value as f32, Ordering::SeqCst);
     }
 
     fn get_raw(&self) -> f64 {
@@ -91,26 +92,26 @@ impl ClapParam for IntParam {
         self.set_raw(self.denormalize(normalized));
     }
 
-    fn min_value(&self) -> f64 {
-        self.min_value as f64
-    }
-
-    fn max_value(&self) -> f64 {
-        self.max_value as f64
-    }
-
     fn flags(&self) -> ParamInfoFlags {
         self.flags
     }
 
+    fn min_value(&self) -> f64 {
+        self.min_value
+    }
+
+    fn max_value(&self) -> f64 {
+        self.max_value
+    }
+
     fn normalize(&self, value: f64) -> f64 {
-        let range = (self.max_value - self.min_value) as f64;
-        (value - self.min_value as f64) / range
+        let range = self.max_value - self.min_value;
+        (value - self.min_value) / range
     }
 
     fn denormalize(&self, normalized: f64) -> f64 {
-        let range = (self.max_value - self.min_value) as f64;
-        normalized * range + self.min_value as f64
+        let range = self.max_value - self.min_value;
+        normalized * range + self.min_value
     }
 
     fn as_ptr(&self) -> ParamPtr {
@@ -118,7 +119,7 @@ impl ClapParam for IntParam {
     }
 }
 
-impl __ParamInitializer for IntParam {
+impl __ParamInitializer for FloatParam {
     fn __initialize(&mut self, name: String, id: ClapId, module: Option<String>) {
         self.name = name;
         self.id = id;
--- src/params/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod atomic_f32;
-pub mod param_bool;
-pub mod param_enum;
-pub mod param_float;
-pub mod param_int;
