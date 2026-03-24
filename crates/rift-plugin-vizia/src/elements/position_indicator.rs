fdd0b638eeae606603d282f1fae484a72a24e824
sacha-renault <sacha35340@gmail.com>
2026-03-24 13:23:37 +0100

    feat: remake position indicator as a Element so i can have all normal css styling

--- crates/rift-plugin-vizia/src/elements/position_indicator.rs
+++ crates/rift-plugin-vizia/src/elements/position_indicator.rs
@@ -1,4 +1,4 @@
-use vizia::vg;
+use std::marker::PhantomData;
 
 use super::gui_prelude::*;
 
@@ -7,7 +7,7 @@ pub struct PositionIndicator<L>
 where
     L: Lens<Target = f32>,
 {
-    position: L,
+    _p: PhantomData<L>,
 }
 
 impl<L> PositionIndicator<L>
@@ -15,30 +15,18 @@ where
     L: Lens<Target = f32>,
 {
     pub fn new(cx: &mut Context, position: L) -> Handle<'_, Self> {
-        Self { position }
-            .build(cx, |_| {})
+        Self { _p: PhantomData }
+            .build(cx, |cx| {
+                Element::new(cx)
+                    .position_type(PositionType::Absolute)
+                    .height(Percentage(100.))
+                    .left(position.map(|x| Percentage(*x * 100.)))
+                    .class("position-indicator");
+            })
             .pointer_events(false)
             .navigable(false)
             .focusable(false)
     }
 }
 
-impl<L> View for PositionIndicator<L>
-where
-    L: Lens<Target = f32>,
-{
-    fn draw(&self, cx: &mut DrawContext, canvas: &Canvas) {
-        let vt = ViewportTransform::new(cx.bounds());
-        let pos = self.position.get(cx);
-
-        let start = vt.transform(pos, 0.);
-        let end = vt.transform(pos, 1.);
-
-        let mut paint = vg::Paint::default();
-        paint
-            .set_color(cx.font_color())
-            .set_stroke_width(cx.border_width());
-
-        canvas.draw_line(start, end, &paint);
-    }
-}
+impl<L> View for PositionIndicator<L> where L: Lens<Target = f32> {}
