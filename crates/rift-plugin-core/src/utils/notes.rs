3ae05c4c14acfdae76f502c8c3613edf9ef9a261
sacha-renault <sacha35340@gmail.com>
2026-03-30 20:18:10 +0200

    fix (core): rename vars to fit with A5

--- crates/rift-plugin-core/src/utils/notes.rs
+++ crates/rift-plugin-core/src/utils/notes.rs
@@ -1,7 +1,7 @@
-pub const A4_MIDI: f32 = 81.;
-pub const A4_FREQUENCY: f32 = 440.;
+pub const A5_MIDI: f32 = 81.;
+pub const A5_FREQUENCY: f32 = 440.;
 
 pub fn midi_to_frequency(midi_note: u8) -> f32 {
-    let a4_offset = midi_note as f32 - A4_MIDI;
-    A4_FREQUENCY * 2.0_f32.powf(a4_offset / 12.)
+    let offset = midi_note as f32 - A5_MIDI;
+    A5_FREQUENCY * 2.0_f32.powf(offset / 12.)
 }
