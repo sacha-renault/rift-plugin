86eee0151c1e04a855e545e9a1b1097b7f007d86
sacha-renault <sacha35340@gmail.com>
2026-03-30 23:32:48 +0200

    feat (oscillator): add struct that can handle many voices

--- crates/rift-plugin-oscillator/src/lib.rs
+++ crates/rift-plugin-oscillator/src/lib.rs
@@ -1,5 +1,7 @@
+mod oscillator;
 mod oscillator_position;
 
 pub mod prelude {
+    pub use super::oscillator::Oscillator;
     pub use super::oscillator_position::OscillatorPosition;
 }
---
+++ crates/rift-plugin-oscillator/src/oscillator.rs
@@ -0,0 +1,50 @@
+use rift_plugin_core::utils::{bounded_vec::BoundedVec, notes::midi_to_frequency};
+
+use crate::prelude::OscillatorPosition;
+
+pub struct Oscillator {
+    voices: BoundedVec<OscillatorPosition>,
+    active_count: usize,
+}
+
+impl Oscillator {
+    pub fn new(samplerate: f32) -> Self {
+        let mut voices = BoundedVec::new(127);
+        voices.resize_with(127, || OscillatorPosition::new(samplerate));
+        Oscillator {
+            voices,
+            active_count: 0,
+        }
+    }
+
+    pub fn is_active(&self) -> bool {
+        self.active_count != 0
+    }
+
+    pub fn trigger_with_phase<F>(&mut self, note: u8, phase_generator: F)
+    where
+        F: FnOnce() -> f32,
+    {
+        let frequency = midi_to_frequency(note);
+        self.voices[note.clamp(0, 126) as usize].trigger_with_phase(frequency, phase_generator);
+    }
+
+    pub fn deactivate(&mut self, note: u8) {
+        self.voices[note.clamp(0, 126) as usize].deactivate();
+    }
+
+    pub fn get_next<F>(&mut self, generator: F) -> f32
+    where
+        F: Fn(f32) -> f32,
+    {
+        let mut value = 0.;
+
+        for voice in self.voices.iter_mut() {
+            if voice.is_active() {
+                value += generator(voice.get_next())
+            }
+        }
+
+        value
+    }
+}
