3bbfcb277a4389ee988595389d56f82df0569ab1
sacha-renault <sacha35340@gmail.com>
2026-04-03 18:45:32 +0200

    refactor (accumulator): use fn in both consumer traits instead of a trait with blanket implementation

--- crates/rift-plugin-accumulator/src/consumers/mod.rs
+++ crates/rift-plugin-accumulator/src/consumers/mod.rs
@@ -1,7 +1,3 @@
-use std::{cell::RefCell, rc::Rc};
-
-use rift_plugin_core::prelude::ConsumerCell;
-
 mod audio_peaks;
 mod consumer_dispatcher;
 mod spectrogram;
@@ -13,20 +9,3 @@ pub use consumer_dispatcher::{ChannelMode, ConsumerDispatcher};
 pub use spectrogram::StftConsumer;
 pub use traits::{MonoConsumer, MultiConsumer};
 pub use windowed_peaks::{Bucket, PeakBucket, WindowBuckets};
-
-pub trait WrapsConsumer {
-    /// Wraps an [`AudioConsumer`] into a [`ConsumerCell`].
-    ///
-    /// **Notes**:
-    /// [`ConsumerCell`] is a pretty name for [`Rc<RefCell<_>>`]
-    fn wraps_consumer(self) -> ConsumerCell<Self>;
-}
-
-impl<T> WrapsConsumer for T
-where
-    T: MonoConsumer,
-{
-    fn wraps_consumer(self) -> ConsumerCell<Self> {
-        Rc::new(RefCell::new(self))
-    }
-}
